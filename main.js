fbf82af27b5d4f1f30647fc70e2994317f1da120
AimeeR74655 <82007449+AimeeR74655@users.noreply.github.com>
2022-08-14 02:47:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
+scoreLeftWrist=0;
+scoreRightWrist=0;
 
 function setup()
 {
@@ -28,18 +30,42 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+
+  
+    if(scoreLeftWrist>0.2)
+    {
+    circle(leftWristX,leftWristY,20);
+    bday_song.isPlaying();
+    dora_song.stop();
+    console.log("Birthday song is now playing")
+    }
+    else(scoreRightWrist>0.2)
+    {
+    circle(rightWristX,rightWristY,20);
+    dora_song.isPlaying();
+    bday_song.stop();
+    console.log("Dora song is now playing")
+
+    }
 }
 
 function preload()
 {
-    dora_song= "dora.mp3";
-    bday_song= "bday.mp3";
+    dora_song= loadSound("dora.mp3");
+    bday_song= loadSound("bday.mp3");
 }
 
 function gotPoses(results)
 {
     if(results.length > 0 )
     { 
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("rightWristX = " + scoreRightWrist + "scorelightWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
